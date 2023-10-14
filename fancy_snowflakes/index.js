ba7a9b78282fcea5385eb0e9124213c04897dbb9
DANLCARTON <ericthiberge@orange.fr>
2023-10-14 11:52:20 +0200

    simplification fancy snowflakes

--- fancy_snowflakes/index.js
+++ fancy_snowflakes/index.js
@@ -1,27 +1,4 @@
 import * as THREE from "three"
-import { OrbitControls } from 'OrbitControls'; // importation de l'addon Orbit Controls pour la gestion de la caméra
-import { TrackballControls } from 'TrackballControls'; // importation de l'addon Orbit Controls pour la gestion de la caméra
-
-// récupération des paramètres dans l'URL
-// const urlParams = new URLSearchParams(window.location.search)
-// let URL_X = urlParams.get("x")
-// let URL_Y = urlParams.get("y")
-// let URL_Z = urlParams.get("z")
-// let URL_MAX_ITER = urlParams.get("iter")
-// let URL_WIREFRAME = urlParams.get("wireframe")
-// let URL_PROBA = urlParams.get("probability")
-// let URL_SHAPE = urlParams.get("shape")
-
-// Valeurs par défaut si aucun paramètre n'est entré dans l'URL
-// if (URL_X == null) URL_X = 2
-// if (URL_Y == null) URL_Y = 2
-// if (URL_Z == null) URL_Z = 2
-// if (URL_MAX_ITER == null) URL_MAX_ITER = 5
-// if (URL_WIREFRAME == null || URL_WIREFRAME != "on") URL_WIREFRAME = false
-// else URL_WIREFRAME = true
-// if (URL_PROBA == null) URL_PROBA = 1
-// else URL_PROBA = parseFloat(URL_PROBA)
-// if (URL_SHAPE == null) URL_SHAPE == "cube"
 
 const URL_X = 2
 const URL_Y = 2
@@ -66,8 +43,8 @@ const drawCubeAtPosition = (position, iter) => { // Ajout de 'iter' comme argume
     const nb = Math.floor(Math.random() * (URL_PROBA + (iter / 10))) // tire un nombre entre 0 et un nombre donné dans les paramètres avant. arrondi à l'unité inférieure
     if (nb == 0 || iter == 1) { // si le nombre tiré est = à 0 ou si on est à la première itération (pour éviter d'avoir un rendu vide)
         const cube = new THREE.Mesh(geometry, material) // on définit un nouveau mesh
-        cube.castShadow = true; // il pourra générer des ombres
-        cube.receiveShadow = true // il pourra recevoir des ombres
+            // cube.castShadow = true; // il pourra générer des ombres
+            // cube.receiveShadow = true // il pourra recevoir des ombres
         cube.position.set(position.x, position.y, position.z) // il sera à la position donnée en paramètres
         cube.scale.set( // rétrécissement du mesh par rapport à chaque itération de manière à ce qu'il soient de plus en plus petits
             1 / Math.pow(URL_X, iter),
@@ -105,57 +82,10 @@ if (URL_WIREFRAME) { // choix des paramètres du matériau (wireframes ou pas)
 }
 const material = new THREE.MeshPhongMaterial(materialProperties)
 
-// définition de la scene et de la caméra
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5
 let fancySnowflake = new THREE.Mesh()
-const renderer = new THREE.WebGLRenderer();
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-// lumières
-scene.add(new THREE.AmbientLight(0xd2b48c, .5))
-
-const point = new THREE.PointLight(0xff8888, 12)
-point.position.set(0, 2, 0)
-point.castShadow = true
-camera.add(point)
-
-const point2 = new THREE.PointLight(0x88ff88, 12)
-point2.position.set(0, -2, 0)
-point2.castShadow = true
-camera.add(point2)
-
-const point3 = new THREE.PointLight(0x8888ff, 12)
-point3.position.set(2, 0, 0)
-point3.castShadow = true
-camera.add(point3)
-
-const point4 = new THREE.PointLight(0xffff88, 12)
-point4.position.set(-2, 0, 0)
-point4.castShadow = true
-camera.add(point4)
 
 // premier appel de la fonction récursive
 var iter = 1
 drawCubeAtPosition({ x: 0, y: 0, z: 0 }, iter)
-
-// définition des contrôles de la caméra
-const controls = new TrackballControls(camera, renderer.domElement);
-scene.add(camera)
-
-scene.add(fancySnowflake)
-
-// boucle de rendu
-function animate() {
-    requestAnimationFrame(animate);
-    controls.update()
-    renderer.render(scene, camera);
-}
-
-animate();
 
 export { fancySnowflake }
